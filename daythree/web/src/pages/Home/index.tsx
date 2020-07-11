ff1a6eb29d5602c48193bbc8242eafdebfaac38a
glaulher <glaulher.developer@gmail.com>
2020-07-11 09:23:15 -0300

    day tree

--- daythree/web/src/pages/Home/index.tsx
+++ daythree/web/src/pages/Home/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from '../../assets/logo.svg';
 import './styles.css';
 import { FiLogIn } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 
 const Home = (): JSX.Element => {
   return (
@@ -16,12 +17,12 @@ const Home = (): JSX.Element => {
             Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.
           </p>
 
-          <a href="/create-point">
+          <Link to="/create-point">
             <span>
               <FiLogIn />
             </span>
             <strong>Cadastre um ponto de coleta</strong>
-          </a>
+          </Link>
         </main>
       </div>
     </div>
--- daythree/web/src/routes.tsx
+++ daythree/web/src/routes.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { Route, BrowserRouter } from 'react-router-dom';
 
 import Home from './pages/Home';
-//import CreatePoint from './pages/CreatePoint';
+import CreatePoint from './pages/CreatePoint';
 
 const Routes = (): JSX.Element => {
   return (
     <BrowserRouter>
-      <Route component={Home} path="/" />
+      <Route component={Home} path="/" exact />
+      <Route component={CreatePoint} path="/create-point" />
     </BrowserRouter>
   );
 };
